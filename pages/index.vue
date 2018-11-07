1275e7c377cfedf67ce794db50b333346b33ea20
Brian Norris <bdnorris@gmail.com>
2018-11-06 20:21:16 -0600

    Getting started with Nuxt

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+
+      <nuxt-link to="/projects">Projects</nuxt-link>
+
+      <h1 class="title">
+        Brian Norris
+      </h1>
+      <h2 class="subtitle">
+        Web Designer/Developer
+      </h2>
+      <p>
+        Hi, I’m Brian Norris. I have 16 years experience working for advertising and media agencies as a graphic designer and web developer. I’m currently a Web Developer at The Marlin Network in Southwest Missouri and instruct Web Design courses for design majors at Missouri State University.
+      </p>
+      <p>
+        I work with graphic designers, engineers, copywriters and user experience designers to deliver functional user experiences on the web. Find me through the various channels below, or take a look at some of my educational resources or my extra-curricular projects.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ pages/projects.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>projects</h1>
+    <div>
+      <nuxt-link to="/">Home</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    // Logo
+  }
+}
+</script>
