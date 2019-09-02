39afdbb10b2a0f464abe0610bfe091e608e39fa8
Brian Norris <bdnorris@gmail.com>
2019-09-02 15:20:28 -0500

    Panel work

--- components/Panel.vue
+++ components/Panel.vue
@@ -13,6 +13,13 @@
       </h2>
       <template v-if="panelSlug === 'balls'">
         <p>"Because George wants to invite him to the ball," said Fred sarcastically. "Because we want to send a letter, you stupid great prat," said George. "Who d'you two keep writing to, eh?"  said Ron. "Nose out, Ron, or I'll burn that for you too," said Fred, waving his wand threateningly. "So . . . you lot got dates for the ball yet?" </p>
+        <p>"Because George wants to invite him to the ball," said Fred sarcastically. "Because we want to send a letter, you stupid great prat," said George. "Who d'you two keep writing to, eh?"  said Ron. <a href="#">"Nose out, Ron, or I'll</a> burn that for you too," said Fred, waving his wand threateningly. "So . . . you lot got dates for the ball yet?" </p>
+        <ul>
+          <li>
+            <a href="#">Code Pen</a>
+            <a href="#">Project Page</a>
+          </li>
+        </ul>
       </template>
     </article>
   </div>
@@ -50,20 +57,33 @@ export default {
 <style scoped lang="scss">
 
 @import '../assets/variables.scss';
+@import '../assets/mixins.scss';
 
-article {
-  // background-color: $second;
-  // position: absolute;
-  // top: 0;
-  // left: 0;
-  // right: 0;
-  // bottom: 0;
+.panel {
+  width: 100%;
+  height: 100%;
+  flex: 1 0 70%;
+  background-color: $beige;
+  position: relative;
+  padding: 1em 2.5em 2em 2em;
   h2 {
-    color: $second;
+    color: $denim-light;
+    font-size: 3em;
+    line-height: 1;
   }
   p {
     color: $text;
+    &:first-of-type {
+      margin-top: 1.5em;
+    }
   }
+}
+
+.close-button {
+  @include button-reset;
+  position: absolute;
+  top: 2em;
+  right: 2em;  
 }
 
 </style>
